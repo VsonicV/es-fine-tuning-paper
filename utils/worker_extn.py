54a51df99839cf6cce24d7d6f66976e56b618184
dibbla <120090375@link.cuhk.edu.cn>
2025-11-07 06:51:19 +0000

    add eval code for vllm

--- utils/worker_extn.py
+++ utils/worker_extn.py
@@ -69,3 +69,12 @@ class WorkerExtension:
             torch.cuda.empty_cache()
         time.sleep(0.1)
         return True
+
+    def load_weights_from_disk(self, filepath):
+            state_dict = torch.load(filepath, map_location=self.device)
+            self.model_runner.model.load_state_dict(state_dict)
+            gc.collect()
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+            time.sleep(0.1)
+            return True
